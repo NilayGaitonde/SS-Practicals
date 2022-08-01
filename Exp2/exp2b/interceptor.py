5d82eae170ca563de4bdeeea6d8e11fffcf1f8aa
Nilay Gaitonde <68230775+NilayGaitonde@users.noreply.github.com>
2022-08-01 23:09:36 +0530

    Modification done

--- Exp2/exp2b/interceptor.py
+++ Exp2/exp2b/interceptor.py
@@ -17,6 +17,7 @@ class Interceptor:
             s.bind((host,port))
             s.listen()
             conn,addr = s.accept()
+            newMessage = input("Enter message you want to send:")
             with conn:
                 while True:
                     data = conn.recv(1024)
@@ -26,21 +27,13 @@ class Interceptor:
                     print(f"Received: {data}")
                     data = cipher.decrypt("four",data)
                     print(f"Message: {data}")
-                    data = cipher.encrypt("four",data)
+                    data = cipher.encrypt("four",newMessage)
                     print(f"Sent: {data}")
                     break
         self.sendServer('127.0.0.1',6666,data)
 
     def sendServer(self,host,port,message):
         print("Interceptor is sending data to the server...")
-        with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-            s.connect((host,port))
-            s.sendall(message.encode())
-            data = s.recv(1024)
-            print(f"Data:{data.decode()}")
-        
-    def sendUser(self,host,port,message):
-        print("Interceptor is sending data to the user...")
         with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
             s.connect((host,port))
             s.sendall(message.encode())
